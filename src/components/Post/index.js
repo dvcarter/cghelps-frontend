853bb72aa9244360ef61af7ad17e40599d75083d
Desmond Carter <crtr@dsmnd.org>
2017-08-14 23:51:59 -0400

    Add dispatch and state mapping to post component

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as actions from "../../postActions";
 
-const Post = (props) => (
-        <div> 
-            <p> This is where posts go </p>
-        </div>
-)
+class Post extends Component {
+    componentWillMount() {
+        this.props.actions.loadPosts();
+    }
 
+    render() {
+        const i = this.props.posts;
+        return (
+            <div> 
+                <p> This is where posts go </p>
+                    <div key ={i}>
+                        something here
+                    </div>
+            </div>
+        )
+    }
+}
 
-export default Post
+function mapStateToProps(state, ownProps) {
+    return {
+        posts: state.posts
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return { actions: bindActionCreators(actions, dispatch)}
+}
+
+Post.propTypes = {
+    posts: PropTypes.array.isRequired,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
