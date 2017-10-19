2940af0f1f5b6760bf7d74ef01a801aeb9545341
Desmond Carter <crtr@dsmnd.org>
2017-10-19 10:25:53 -0400

    Update Post hero

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import {Link} from "react-router-dom";
 import "./findIndexHelper.js";
 import styles from "./index.css"
+import sty_post from "./postStyles.css"
 
 class Post extends Component {
     
@@ -9,15 +10,25 @@ class Post extends Component {
             const { slug } = this.props.match.params;
             const i = this.props.posts.findIndex((post) =>  post.slug === slug)
             const post = this.props.posts[i]
+
             return (
-                <div>
-                        <div>
+                <div className={styles.superHolder_Post}>
+                    <div>
+                               <div
+                                    className={ sty_post.hero_0 } 
+                                    style={ {background: `#111 url(${post.acf.hero.sizes.large}) 50% 50% / cover`}}
+                                >   
+                                    <div className={ sty_post.header_0 }>
+                                        <div className={ sty_post.wrapper_0 }>
+                                            <div className={ sty_post.heading_0 }>{post.title.rendered} </div>
+                                            <div className={ sty_post.lowerTitleHolder_0}>
+                                                {/*{getHelps.heroSubtitle} */}
+                                            </div>
+                                        </div>
+                                    </div>
+                            </div>
                             <h2> {post.title.rendered} </h2>
-                           <div>
-                           {/* <h1> { posts[0].title.rendered} </h1> */}
-                           </div>
                            <div> 
-                             <img className={styles.imgs} src={post.acf.hero.sizes.large} alt="" />
                              <div className={styles.description} dangerouslySetInnerHTML={{__html:post.content.rendered}} /> 
                              <div> {post.categories} </div>
                            </div>
