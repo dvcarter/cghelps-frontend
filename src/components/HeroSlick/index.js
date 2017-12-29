38c90a5cd4cd4499f6819840e2e5df76379c4fd4
Desmond Carter <crtr@dsmnd.org>
2017-12-29 14:12:51 -0500

    update package.json file

--- src/components/HeroSlick/index.js
+++ src/components/HeroSlick/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react"
 import ReactDOM from "react-dom"
 import {Link} from "react-router-dom"
+import Slider from "react-slick"
 import styles from "./index.css"
 import PSA from "../../content/assets/psa.png"
 import seekHelp from "../../content/assets/seekhelp.jpg"
@@ -10,7 +11,6 @@ import SSU from "../../content/assets/grouphero.jpg"
 import getInvolved from "../../content/assets/getinvolved.jpg"
 import sty from "./heroSliderIdentifier.css"
 
-var Slider = require('react-slick')
 
 
 function SampleNextArrow(props) {
