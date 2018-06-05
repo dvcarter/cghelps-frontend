e216d2765eed4f4a40be3ec4d843b53270c2ee5e
Desmond Carter <crtr@dsmnd.org>
2018-06-05 17:00:55 -0400

    Message regarding the passing of Kate Spade

--- src/components/LatestPosts/index.js
+++ src/components/LatestPosts/index.js
@@ -14,10 +14,9 @@ const LatestPosts = () => {
                             {/* {posts.title.rendered} */}
                                 <img className={styles.standInImg} src={bird} alt="" />
                                 <div className={styles.textArea}> 
-                                    <h4 className={styles.subtitle}>Legal Clinic closed due to Memorial Day</h4>
+                                    <h5 className={styles.subtitle}>Businesswoman’s Death Highlights Mental Health Risks for Women </h5>
                                     <div className={styles.descriptionArea}> 
-                                        Due to the Memorial Day holiday, Legal Clinic is closed today, May 29<sup>th</sup>. It will be in session on Thursday, May 31<sup>st</sup>...
-            
+           We're saddened to hear of Kate Spade's death and our condolences go out to her friends, family, and coworkers. We encourage everyone to be kind, to check in on your loved ones... 
                                     </div>
                                 <Link to="/posts"> Read more </Link>
                                 </div>
